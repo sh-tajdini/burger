b8b5d935635eea2a6da764323a5cfffd32d66de6
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-11 17:09:40 -0400

    creat builder component and create layout css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-
+import BurgurBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./components/Layout/Layout";
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgurBuilder />
         </Layout>
       </div>
     );
