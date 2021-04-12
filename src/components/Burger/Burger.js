52b20cae73fa0ca143f59ee1799a8a106d6c4146
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-12 18:54:16 -0400

    relation to burgerbulde-create burger& ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,12 +1,22 @@
 import React from "react";
 import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
 const burger = (props) => {
+  //change object which is comming from burger builder to array
+  const transformedIngredients = Object.keys(props.ingredients) // salad ,bacon , cheese
+    //dar inja igkey ke az bala miad hamoon salad o chizo o gheyre ast ke felan tedad nadarad
+
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])] //[,]
+        .map((_, i) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
