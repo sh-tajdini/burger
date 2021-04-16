d5e8a503af406c9b2a2ed085a01215f56eb132b2
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-16 16:57:53 -0400

    Responsive Adjusment of logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,10 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
 
-    <Logo height="80%" />
     <nav>
       <NavigationItems />
     </nav>
