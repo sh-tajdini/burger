98df99accc35c65bdf6249e752189a3c8e5a38bb
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-17 11:09:00 -0400

    change modal to class

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.css";
 import Aux from "../../../hoc/Aux";
 import Bakcdrop from "../Backdrop/Backdrop";
-const modal = (props) => (
-  <Aux>
-    <Bakcdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      //set visible or not
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  render() {
+    return (
+      <Aux>
+        <Bakcdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          //set visible or not
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
