789452b5dc7c9e450ab87765e583b7e0bf0e70ea
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-20 19:47:05 -0400

    compoentwill unmount & test after 5s in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import BurgurBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
 class App extends Component {
+  state = {
+    show: true,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: false });
+    }, 5000);
+  }
   render() {
     return (
       <div>
-        <Layout>
-          <BurgurBuilder />
-        </Layout>
+        <Layout>{this.state.show ? <BurgurBuilder /> : null}</Layout>
       </div>
     );
   }
