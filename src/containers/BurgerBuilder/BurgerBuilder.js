fa4cc6b92f161e2848e85b64b84b046f7467d413
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-26 19:31:15 -0400

    pass the ingreedients to search property in address bar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -116,8 +116,20 @@ class BurgerBuilder extends Component {
     //   });
     // // alert("You continue");
 
-    //switch the page
-    this.props.history.push("/checkout");
+    //switch the page & pass ingredients to search
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString,
+    });
   };
 
   render() {
