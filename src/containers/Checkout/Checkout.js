d3445bfc9fcc9898fcb6a32922aa94fc6ea8f5b5
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-27 20:17:37 -0400

    change the styling of contact data

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 class Checkout extends Component {
   state = {
     ingredients: {
@@ -27,7 +29,7 @@ class Checkout extends Component {
   };
 
   checkoutContiniuedHandler = () => {
-    this.props.history.replace("/chechout/contact-data");
+    this.props.history.replace("/checkout/contact-data");
   };
   render() {
     return (
@@ -36,6 +38,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContiniued={this.checkoutContiniuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
         />
       </div>
     );
