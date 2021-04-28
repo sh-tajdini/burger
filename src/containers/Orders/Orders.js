1e6fdf33aecc6c757074eb78afb332844f6e18d5
Shervin Tajdini <shervin.taj@gmail.com>
2021-04-28 16:13:36 -0400

    fetchin orders and using error handler warrping component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get("/orders")
+      .then((res) => {
+        // console.log(res.data);
+        //changein object to array
+        const fetchOrders = [];
+        for (let key in res.data) {
+          fetchOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        this.setState({ loading: false, orders: fetchOrders });
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -12,4 +38,4 @@ class Orders extends Component {
     );
   }
 }
-export default Orders;
+export default withErrorHandler(Orders, axios);
